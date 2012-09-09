3de07555b6487bfa89f5a3d3b38a36480a0db687
Jelle van der Waa <jelle@vdwaa.nl>
2012-09-09 23:14:25 +0200

    added getorphans
    Signed-off-by: Jelle van der Waa <jelle@vdwaa.nl>

--- pyflyspray.py
+++ pyflyspray.py
@@ -9,12 +9,13 @@ from mod_pyflyspray import *
 
 if __name__ == "__main__":
     Trackers = ['Archlinux','Community','AUR','Pacman','ReleaseEngineering']
+    Repo = ['Core','Testing','Community','Community-Testing','Extra','All']
     parser = argparse.ArgumentParser(description='Interface to the Archlinux Bugtracker')
     parser.add_argument('-u','--unassigned', help='Fetch unassigned bugs from Archlinux,AUR,Community,Pacman or ReleaseEngineering',choices=Trackers, required=False)
     parser.add_argument('-a','--assigned', help='Fetch assigned bugs by given maintainer from bugs.archlinux.org', required=False)
     parser.add_argument('-o','--openbugs', help='Fetch open bugs since given date yyyy-mm-dd and tracker',required=False,nargs=2)
     parser.add_argument('-b','--orphanbugs', help='Fetch orphan bugs from Archlinux or Community',choices=Trackers[:2], required=False)
-    parser.add_argument('-s','--orphans', help='Fetch orphans from Archlinux or Community', choices=Trackers[:2], required=False)
+    parser.add_argument('-s','--orphans', help='Fetch orphans from Archlinux or Community', choices=Repo, required=False)
     parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 
     args = vars(parser.parse_args())
@@ -28,6 +29,8 @@ if __name__ == "__main__":
     elif args['orphanbugs']:
         print (bt.getorphanbugs(args['orphanbugs']))
     elif args['orphans']:
-        print (bt.getorphans(args['orphans']))
+        orphans = (getorphans(args['orphans']))
+        for orphan in orphans:
+            print ("- {} ".format(orphan))
     else:
         parser.print_help()
